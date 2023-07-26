c02137e41c189e983425f40c333b598d6bb4744b
kurtprime <kurtquejada@gmail.com>
2023-07-26 14:56:10 +0800

    portfolio showcase

--- components/Transition.js
+++ components/Transition.js
@@ -26,7 +26,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.2, duration: 0.4, ease: 'easeInOut' }}
+        transition={{ delay: 0.1, duration: 0.3, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-[90] bg-[#3b2d71]"
@@ -34,7 +34,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.3, duration: 0.4, ease: 'easeInOut' }}
+        transition={{ delay: 0.2, duration: 0.3, ease: 'easeInOut' }}
       ></motion.div>
       <motion.div
         className="fixed top-0 bottom-0 right-full w-screen h-screen z-[80] bg-[#4b3792]"
@@ -42,7 +42,7 @@ const Transition = () => {
         initial="initial"
         animate="animate"
         exit="exit"
-        transition={{ delay: 0.4, duration: 0.4, ease: 'easeInOut' }}
+        transition={{ delay: 0.3, duration: 0.3, ease: 'easeInOut' }}
       ></motion.div>
     </>
   );
